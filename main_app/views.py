bfaad3059a3bac7815c24d9dc8b6f16ddb18895d
Tirapat <mind1990@gmail.com>
2018-10-18 16:00:16 -0700

    connected Cat model to view.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,28 @@
+# A view is a function that takes in a web request and returns a web response.
+
 # Create your views here.
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # def index(request):
 	# return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 
 from django.shortcuts import render
+from .models import Cat
 
 # In url.py this is views.index OR localhost:8000/index in browser
 def index(request):
+	cats = Cat.objects.all()
 	return render(request, 'index.html', {'cats': cats})
 
 
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+# class Cat:
+#   def __init__(self, name, breed, description, age):
+#     self.name = name
+#     self.breed = breed
+#     self.description = description
+#     self.age = age
 
 
 cats = [
