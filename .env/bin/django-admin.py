9e5def439f4f0a780df81b3d3006adc4fc8b5c7d
Tirapat <mind1990@gmail.com>
2018-10-18 11:50:21 -0700

    initialized

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tn/Documents/General-Assembly/WDI-48/Week-6/ThuOct18/cat-collectr/.env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
