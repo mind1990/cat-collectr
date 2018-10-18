a54bb0436436c1531fcbf5a69b7d44a5de8931f9
Tirapat <mind1990@gmail.com>
2018-10-18 14:21:59 -0700

    cat list added

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-
 # Create your views here.
+
 from django.http import HttpResponse
 
+# def index(request):
+	# return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+from django.shortcuts import render
 
 # In url.py this is views.index OR localhost:8000/index in browser
 def index(request):
-	return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+	return render(request, 'index.html', {'cats': cats})
+
+
+class Cat:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
